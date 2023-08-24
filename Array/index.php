ca7fd52e2257d2a61c11d0d4a2de8a697fc22cf4
brunovicenters <brunovicente.dev@gmail.com>
2023-08-24 11:30:14 -0300

    Added array functions

--- Array/index.php
+++ Array/index.php
@@ -60,4 +60,66 @@
     echo 'A quantidade de animais é: '.count($animais);
     echo "<br>";
     echo 'A quantidade de cervejas é: '.count($cervejas);
+    echo "<hr>";
+
+    //*******************************************************************************************
+
+    // Functions with array --
+    $nomes = ["Pai"=>"João", "Irmão"=>'Pedro', "Primo"=>'Carlos', "Mãe"=>'Maria'];
+
+    // It checks if it's a array--
+    echo is_array($nomes); // -- 1 it's true, 0 it's false;
+    echo '<hr>';
+    var_dump($nomes);
+    echo '<hr>';
+
+    // It checks if exists in the array--
+    echo in_array("Carlos", $nomes);
+    echo '<hr>';
+    if(in_array("Rodrigo", $nomes)){
+        echo "There's a name like this here";
+    } else {
+        echo "There's no name like this here";
+    }
+    echo '<hr>';
+
+    // It saves the keys of an array -- 
+    $subjects = ['DD', 'CMS', 'PHP', 'JS'];
+    $keys = array_keys($subjects);
+    print_r($keys);
+    echo '<hr>';
+    $family = array_keys($nomes);
+    print_r($family);
+    echo '<hr>';
+
+    //*******************************************************************************************
+
+    // Exercise:
+
+    $arr1 = ["a"=> "cadeira", "2"=> "mesa", "3"=> "balcão"];
+    $arr2 = ["a"=> "sofá", "b"=> "tv", "c"=> "tapete"];
+
+    if(array_keys($arr1) === array_keys($arr2)){
+        echo "Keys are identical";
+    } else {
+        echo "Keys aren't identical";
+    }
+    echo "<hr>";
+    
+    //*******************************************************************************************
+
+    $veiculos = ["opala", "corcel", "chevett"];
+    $anos = ["1975", '1980', "1993"];
+    // You merge the arrays --
+    $merge = array_merge($veiculos, $anos);
+    print_r($merge);
+    echo "<hr>";
+
+    //*******************************************************************************************
+
+    // Exercise:
+
+    $infoAntiga = ["Tel"=>"55 11 99022-5294", "End."=>"Avenida Interlagos, 871", "Owner"=>"Robert Renan"];
+    $infoNova = ["Tel"=>"55 11 99530-5370", "Spouse"=>"Marie Jane"];
+    print_r(array_merge($infoAntiga, $infoNova));
 ?>
