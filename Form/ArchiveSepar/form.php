561f8050340d9dcaa8badbb8f7c53003e60e27a7
brunovicenters <brunovicente.dev@gmail.com>
2023-09-01 15:22:05 -0300

    First commit

---
+++ Form/ArchiveSepar/form.php
@@ -0,0 +1,10 @@
+<?php 
+    header('Content-Type: text/html; charset=utf-8');
+    // isset() verifies if exists and empty() verifies if its empty --
+    if(isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['email']) && !empty($_POST['email'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+
+        echo "Nome: $name <br> E-mail: $email";
+    }
+?>
---
+++ Form/ArchiveUnic/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name"> <br>
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="email"> <br>
+        <button  type="submit">Submit</button>
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_GET['name']) && isset($_GET['email'])){
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        echo "Nome: $name <br> E-mail: $email";
+    }
+?>
