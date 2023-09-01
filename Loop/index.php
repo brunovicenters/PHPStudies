fa60f01d7b7b5096b14f27d3721ef59f9087bcfe
brunovicenters <brunovicente.dev@gmail.com>
2023-09-01 15:21:35 -0300

    Exercises & added continue

--- Loop/index.php
+++ Loop/index.php
@@ -79,7 +79,8 @@
 
     // Exercise --
     
-    $input = "5";
+    $input = "12345";
+    // Divide a string into a array with the caracters --
     $digits = str_split($input);
     $soma = 0;
 
@@ -90,4 +91,64 @@
     echo $soma;
 
     echo "<hr>";
+
+    // Another solution --
+
+    $N = 12345;
+    $soma = 0;
+
+    while($N>0){
+        $soma += $N%10;
+        // Gets only the int part of the number -- 
+        $N = (int)($N/10);
+    }
+
+    echo "A soma dos dígitos é $soma <hr>";
+
+    //***********************************************************************************
+
+    // Foreach --
+    $colors = ["blue", "red", "green"];
+    // It will loop for each element on array (like a .map on JS) --
+    foreach($colors as $color){
+        echo "Color $color <br>";
+    }
+
+    echo "<hr>";
+
+    $cars = ["fusca", "sandero", "camaro"];
+    // You get the key and the value --
+    foreach($cars as $key => $car){
+        echo "$key - $car <br>";
+    }
+
+    echo "<hr>";
+
+    //***********************************************************************************
+    
+    // Exercise --
+    $numbers = [1,2,3,4,5];
+    $numbersInverso = [];
+
+    $index = count($numbers);
+
+    foreach($numbers as $number){
+        $index--;
+        $numbersInverso [] = $numbers[$index];
+    }
+    print_r($numbersInverso);
+
+    echo "<hr>";
+
+    //***********************************************************************************
+
+    // Continue --
+    for($x=0; $x<4;$x++){
+        if($x == 2){
+            // Goes back to the beggining --
+            continue; // -- You can use "break;" to end the loop; 
+        }
+        echo $x."<br>";
+    }
+
 ?>
