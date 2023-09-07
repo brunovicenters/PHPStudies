09240ca49cce228244220755161ed589674b53b1
brunovicenters <brunovicente.dev@gmail.com>
2023-09-06 21:09:17 -0300

    Added comment

--- Form/ArchiveUnic/index.php
+++ Form/ArchiveUnic/index.php
@@ -16,6 +16,7 @@
 </body>
 </html>
 <?php
+    // isset() verifies if exists --
     if(isset($_GET['name']) && isset($_GET['email'])){
         $name = $_GET['name'];
         $email = $_GET['email'];
