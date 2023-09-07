577d0fa356b0d2c5ac3999eee87bd85c98b51e02
brunovicenters <brunovicente.dev@gmail.com>
2023-09-06 21:42:42 -0300

    Correction

--- Sanitize/Input/Exercise/index.php
+++ Sanitize/Input/Exercise/index.php
@@ -1,12 +1,15 @@
 <?php
-    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    if(isset($_POST['name'], $_POST['email'], $_POST['msg']) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['msg'])){
+        
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+        $msg = filter_input(INPUT_POST, "msg", FILTER_SANITIZE_STRING);
 
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-
-    $msg = filter_input(INPUT_POST, "msg", FILTER_SANITIZE_STRING);
-
-    if(!empty($name) && !empty($email) && !empty($msg)){
-        echo "Name: $name <br> Email: $email <br> Mensagem: $msg";
+        if($email === false){
+            echo "Enter valid email!";
+        } else {
+            echo "Name: $name <br> Email: $email <br> Mensagem: $msg";
+        }
     } else {
         echo "Please, do not leave any input empty";
     }
