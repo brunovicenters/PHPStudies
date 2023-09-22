4b0d2f6c96c4531b25dfdbe6e065f46dde73b08f
bruno <brunovicente.dev@gmail.com>
2023-09-22 14:27:54 -0300

    Added edit and delete button

--- Project-LoginPage/crud/admin_crud/index_admin.php
+++ Project-LoginPage/crud/admin_crud/index_admin.php
@@ -26,6 +26,18 @@ $admins = $query->fetchAll(PDO::FETCH_ASSOC);
         <div class="row">
             <h1 class="h1 text-center mt-3">Admins on DataBase:</h1>
             <div class="container col-md-4 col-md-offset-4 text-center">
+                <?php if (isset($_GET['warningDel'])) { ?>
+                    <p class="text-warning">Admin deleted!</p>
+                <?php } ?>
+                <?php if (isset($_GET['errorDel'])) { ?>
+                    <p class="text-danger">Admin don't exist!</p>
+                <?php } ?>
+                <?php if (isset($_GET['errorEdit'])) { ?>
+                    <p class="text-danger">There is no id like this</p>
+                <?php } ?>
+                <?php if (isset($_GET['successEdit'])) { ?>
+                    <p class="text-success">Succesfuly updated admin</p>
+                <?php } ?>
                 <table class="table">
                     <thead>
                         <tr>
@@ -33,6 +45,7 @@ $admins = $query->fetchAll(PDO::FETCH_ASSOC);
                             <th scope="col">Id</th>
                             <th scope="col">User</th>
                             <th scope="col">Active</th>
+                            <th scope="col" colspan="2"></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -42,15 +55,26 @@ $admins = $query->fetchAll(PDO::FETCH_ASSOC);
                         ?>
                             <tr>
                                 <th scope="row"><?= $counter ?></th>
-                                <td><?= $admin['id'] ?> </td>
+                                <td><?= $admin['id'] ?></td>
                                 <td><?= $admin['admin_user'] ?></td>
                                 <td><?= $admin['admin_active'] ?></td>
+                                <td>
+                                    <a href="/PHPStudies/Project-LoginPage/crud/admin_crud/edit_admin.php?id=<?= $admin['id'] ?>" class="btn btn-outline-warning">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="/PHPStudies/Project-LoginPage/crud/admin_crud/delete_admin.php?id=<?= $admin['id'] ?>" class="btn btn-outline-danger">
+                                        <i class="bi bi-trash"></i>
+                                    </a>
+                                </td>
                             </tr>
                         <?php
                             $counter++;
                         endforeach
                         ?>
                     </tbody>
+                </table>
             </div>
         </div>
     </div>
