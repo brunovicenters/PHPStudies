df4bb9cefc9b37d75ff14c75dc3b171daf2ecc59
bruno <brunovicente.dev@gmail.com>
2023-09-21 23:38:03 -0300

    fixed miswriting + added button

--- Project-LoginPage/admin/admin_panel.php
+++ Project-LoginPage/admin/admin_panel.php
@@ -3,7 +3,7 @@ session_start();
 
 if (!isset($_SESSION["admin_login"])) {
     // redirect to another location --
-    header("Location:Login.php");
+    header("Location:login.php");
     exit();
 }
 
@@ -15,8 +15,9 @@ if (!isset($_SESSION["admin_login"])) {
 
 <body>
     <h1 class="h1 text-center mt-3">Welcome, Admin!</h1>
-    <div class="container d-flex justify-content-center">
+    <div class="container d-flex flex-column gap-2 justify-content-center col-md-2">
         <a type="btn" class="btn btn-info text center" href="register_product.php">Register product</a>
+        <a type="btn" class="btn btn-warning text center" href="register_admin.php">Register admin</a>
     </div>
 </body>
 
