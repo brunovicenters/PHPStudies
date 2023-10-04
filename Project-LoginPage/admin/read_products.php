fbcc3ed42f76b5374569b09398638270c921022f
bruno <brunovicente.dev@gmail.com>
2023-10-04 20:01:56 -0300

    Changed order + Added buttons delete e edit

--- Project-LoginPage/admin/read_products.php
+++ Project-LoginPage/admin/read_products.php
@@ -40,10 +40,12 @@ try {
                     <thead>
                         <tr>
                             <th scope="col">Id</th>
+                            <th scope="col">Image</th>
                             <th scope="col">Name</th>
                             <th scope="col">Description</th>
                             <th scope="col">Price</th>
-                            <th scope="col">Image</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -53,19 +55,48 @@ try {
                             <tr>
                                 <!-- It shows the id of the product -->
                                 <td><?= $product["id"] ?></td>
+                                <!-- It shows the image + assign the link of the product -->
+                                <td><a href="<?= $product["url_img"] ?>" target="_blank"><img src="<?= $product["url_img"] ?>" alt="<?= $product["description"] ?>" width="50"></a></td>
+
                                 <!-- It shows the name of the product -->
                                 <td class="text-uppercase"><?= $product["name"] ?></td>
                                 <!-- It shows the description of the product -->
                                 <td><?= $product["description"] ?></td>
                                 <!-- It shows the price of the product -->
                                 <td>$<?= $product["price"] ?></td>
-                                <!-- It shows the image + assign the link of the product -->
-                                <td><a href="<?= $product["url_img"] ?>" target="_blank"><img src="<?= $product["url_img"] ?>" alt="<?= $product["description"] ?>" width="50"></a></td>
+                                <td>
+                                    <!-- It sends a request to edit page -->
+                                    <a href="edit_product.php?id=<?= $product['id'] ?>" class="btn btn-outline-warning">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </a>
+                                </td>
+                                <td>
+                                    <!-- It sends a request to delete admin -->
+                                    <a class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                        <i class="bi bi-trash"></i>
+                                    </a>
+                                </td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
                 </table>
                 <a href="/PHPStudies/Project-LoginPage/admin/admin_panel.php" class="btn btn-secondary">Go back</a>
+
+                <!-- Modal -->
+                <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="deleteModalLabel">Are you sure you want to delete <?= $product['name'] ?>?</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="delete_product.php?id=<?= $product['id'] ?>" type="btn" class="btn btn-danger">Delete</a>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
