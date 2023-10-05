0473bf73469d506dc614d4ad237194d1da1e3f5c
bruno <brunovicente.dev@gmail.com>
2023-10-05 00:12:03 -0300

    Commented problematic lines + added (maybe) temporary solution

--- Project-LoginPage/admin/edit_product.php
+++ Project-LoginPage/admin/edit_product.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["admin_login"])) {
     exit();
 }
 
-// It checks if the request method is the POST method --
+// It checks if the request method is the GET method --
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
@@ -38,22 +38,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $price = $_POST['price'];
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//     // It checks if the request method is the POST method --
+//     if (!empty($_POST)) {
+//         $id = isset($_POST['id']) ? $_POST['id'] : "";
+//         $name = isset($_POST['name']) ? $_POST['name'] : "";
+//         $description = isset($_POST['description']) ? $_POST['description'] : "";
+//         $price = isset($_POST['price']) ? $_POST['price'] : "";
 
-    try {
-        $sql = "UPDATE products SET name = ?, description = ?, price = ?, WHERE id = ?";
-        $query = $pdo->prepare($sql);
-        $query->execute([$name, $description, $price, $id]);
+//         try {
+//             $sql = "UPDATE products SET name=?, description=?, price=? WHERE id=?";
+//             $query = $pdo->prepare($sql);
+//             $query->execute([$name, $description, $price, $id]);
 
-        header('Location: read_products.php');
-        exit();
-    } catch (PDOException $err) {
-        echo "Error: " . $err->getMessage();
-    }
-}
+//             header('Location: read_products.php');
+//             exit();
+//         } catch (PDOException $err) {
+//             echo "Error: " . $err->getMessage();
+//         }
+//     } else {
+//         header('Location: admin_panel.php');
+//         exit();
+//     }
+// }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,14 +73,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <h1 class="h1 text-center mt-3">Update Product</h1>
     <div class="container">
         <div class="row">
-            <form action="" method="post" class="col-md-12" enctype="multipart/form-data">
+            <form action="update_product.php?id=<?= $product['id'] ?>" method="post" class="col-md-12" enctype="multipart/form-data">
                 <label class="form-label col-md-12" for="name">Name:</label>
                 <input class="form-control col-md-12 mt-2 mb-3" type="text" name="name" id="name" required value="<?= $product["name"] ?>">
                 <label class="form-label col-md-12 mb-2" for="description">Description:</label>
                 <textarea class="form-control col-md-12 mt-2 mb-3" name="description" id="description" cols="30" rows="5" required><?= $product["description"] ?></textarea>
                 <label class="form-label col-md-12" for="price">Price:</label>
                 <input class="form-control col-md-12 mt-2 mb-3" type="number" name="price" id="price" step="0.01" required value="<?= $product["price"] ?>">
-                <button type="submit" class="btn btn-success">Register</button>
+                <button type="submit" class="btn btn-success">Update</button>
             </form>
         </div>
     </div>
